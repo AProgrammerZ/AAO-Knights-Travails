6c858d90093029b4f2ab12d8360eb3c139c7a72f
Aharon <aharonzarnighian@gmail.com>
2020-05-27 11:34:11 -0400

    Finished Phase 1

--- aharon_knights_travails.rb
+++ aharon_knights_travails.rb
@@ -4,5 +4,29 @@ class KnightPathFinder
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
+        @considered_positions = [start_pos]
+    end
+
+    def self.valid_moves(pos)
+        my_row = @start_pos[0]
+        my_col = @start_pos[1]
+        
+        possible_coordinates = [ 
+            [my_row+2, my_col+1]
+            [my_row+1, my_col+2]
+            [my_row-1, my_col+2]
+            [my_row-2, my_col+1]
+            [my_row-2, my_col-1]
+            [my_row-1, my_col-2]
+            [my_row+1, my_col-2]
+            [my_row+2, my_col-1]
+        ]
+    end
+
+    def new_move_positions(pos)
+        valid_moves = self.class.valid_moves(pos)
+        valid_moves.reject! { |pos| @considered_positions.include?(pos) }
+        @considered_positions << valid_moves
+        return valid_moves
     end
 end
