38ee260b00e9ef23f2320fe642fd6495d2b1f3a2
Aharon <aharonzarnighian@gmail.com>
2020-05-27 14:55:01 -0400

    Finished Phase 2 (hopefully)

--- aharon_knights_travails.rb
+++ aharon_knights_travails.rb
@@ -1,11 +1,12 @@
+require "byebug"
 require_relative "./aharon_PolyTreeNode.rb"
 
-class KnightPathFinder
+class KnightPathFinder        
     def initialize(start_pos)
         @start_pos = start_pos
         @root_node = PolyTreeNode.new(start_pos)
         @considered_positions = [start_pos]
-        # self.build_move_tree
+        self.build_move_tree
     end
 
     def self.valid_moves(pos)
@@ -37,4 +38,26 @@ class KnightPathFinder
         valid_moves.each { |pos| @considered_positions << pos }             
         return valid_moves
     end
+
+    def build_move_tree
+        queue = [@root_node]
+
+        until queue.empty?            
+            first_node = queue.shift
+            next_positions = self.new_move_positions(first_node.value)                        
+
+            next_nodes = next_positions.map { |pos| PolyTreeNode.new(pos) }
+            next_nodes.each do |node|
+                node.parent=(first_node)
+            end
+            
+            queue << next_nodes
+            queue.flatten!
+        end
+        # @root_node now has a tree in it (hopefully)   
+    end    
 end
+
+# load "aharon_knights_travails.rb"
+# k = KnightPathFinder.new([2,2])
+# k.build_move_tree
